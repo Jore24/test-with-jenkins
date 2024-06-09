051bc7d6d11215550adfd8063815747be3f0f471
Jorge Ore <jore24@autonoma.edu.pe>
2024-06-09 16:14:40 -0500

    fix html reporter

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
+import { devices as playwrightDevices } from 'playwright';
 
 export default defineConfig({
   timeout: 100000,
-  projects: [
-    {
-      name: 'chromium',
-      use: {...devices['Desktop Chrome']},
-    },
-  ]
+  use: {
+    ...playwrightDevices['Desktop Chrome'],
+  },
+  reporter: [
+    ['list'],
+    ['html', { outputPath: 'playwright-report' }],
+  ],
 });
